99d6fd80bb9b17835c4af641e5b1f7e5a0dd525a
Teff Marin <marin.desarrollo@gmail.com>
2023-03-28 11:41:00 -0500

    Base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { Employee, EmployeesService } from './employees.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-invoicing';
+  employees: Employee[] = [];
+  expanded: Boolean = true;
+  helloWorld() {
+    alert('Hello world!');
+  }
+
+  constructor(service: EmployeesService) {
+    this.employees = service.getEmployees();
+  }
 }
+
+
+
